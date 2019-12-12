41b75f2a5bddd9a090efdbae892f25577efdc745
R <karthikeyan.r02@sap.com>
2019-12-12 11:58:54 +0530

    TLinted

--- tsxpress/src/app.ts
+++ tsxpress/src/app.ts
@@ -1,11 +1,11 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 const port = 3000;
-app.get('/', (req, res) => {
-  res.send('My application is transpiled from app.ts!');
+app.get("/", (req, res) => {
+  res.send("My application is transpiled from app.ts!");
 });
-app.listen(port, err => {
+app.listen(port, (err) => {
   if (err) {
     return console.error(err);
   }
